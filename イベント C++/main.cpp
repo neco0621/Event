19400f7778546f2c0e84763db86906585a5db979
neco0621 <kane263shun@icloud.com>
2024-06-18 17:27:27 +0900

    up

--- イベント C++/main.cpp
+++ イベント C++/main.cpp
@@ -1,7 +1,23 @@
+#include <iostream>
 
-class Simple
-{
+class KeyEvent {
 public:
-	
-   
+    char key;
+    KeyEvent(char k) : key(k) {}
 };
+
+class KeyFilter {
+public:
+    static bool filter(KeyEvent event) {
+        // ここでイベントをフィルタリング
+        return event.key == 'a'; // 'a'キーのイベントのみ通過
+    }
+};
+
+int main() {
+    KeyEvent event('a');
+    if (KeyFilter::filter(event)) {
+        std::cout << "キー'a'が押されました。" << std::endl;
+    }
+    return 0;
+}
